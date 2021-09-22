a445267384ac27e89073ea6d7397d681c1370dc0
Ayan-18 <80191082+Ayan-18@users.noreply.github.com>
2021-09-22 21:04:52 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        OnTaskDoneListener listener = System.out::println;
-        OnTaskErrorListener listener1 = System.out::println;
-        Worker worker = new Worker(listener, listener1);
+        OnTaskDoneListener listener1 = System.out::println;
+        OnTaskErrorListener listener2 = System.out::println;
+        Worker worker = new Worker(listener1, listener2);
         worker.start();
     }
 }
